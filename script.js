8848965492e165f5d1cae9bce155d39f9cbe980d
siafromspace <sharonyak15@gmail.com>
2022-07-09 11:39:29 +0100

    dlt modal functionality

--- script.js
+++ script.js
@@ -6,26 +6,38 @@ const cancelModal = document.querySelector('.cancel-btn')
 const sendBtn = document.querySelectorAll('.send-btn')
 const newComment = document.querySelector('.current')
 const userComment = document.getElementById('userComment')
+const delComment = document.querySelector('.delete-btn')
 
 //del modal visibilty
 delBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
-        dltModal.style.visibility = "visible"
-        overlay.style.visibility = "visible"
-    })
+  btn.addEventListener('click', delModal)
+  delComment.addEventListener('click', () => {
+    const target = btn.parentNode.parentNode.parentNode
+    console.log(target)
+    target.style.visibility = "hidden"
+    target.style.width = 0
+    target.style.height = 0
+    dltModal.style.visibility = "hidden"
+    overlay.style.visibility = "hidden"
+  })
 })
+
+function delModal() {
+  dltModal.style.visibility = "visible"
+  overlay.style.visibility = "visible"
+}
 
 //remove modal visibility 
 cancelModal.addEventListener('click', () => {
-    dltModal.style.visibility = "hidden"
-    overlay.style.visibility = "hidden"
+  dltModal.style.visibility = "hidden"
+  overlay.style.visibility = "hidden"
 })
 
 // add new comment
 sendBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
-        newComment.insertAdjacentHTML('beforeend',
-            `<div class="main-comment">
+  btn.addEventListener('click', () => {
+    newComment.insertAdjacentHTML('beforeend',
+      `<div class="main-comment">
               <div class="vote-comment">
                 <div class="vote">
                   <i class="fa-solid fa-minus"></i>
@@ -33,7 +45,7 @@ sendBtn.forEach(btn => {
                   <i class="fa-solid fa-plus"></i>
                 </div>
                 <div class="buttons-mobile">
-                  <button class="del-btn"><img src="images/icon-delete.svg" alt="">Delete</button>
+                  <button class="del-btn" onclick="delModal()"><img src="images/icon-delete.svg" alt="">Delete</button>
                   <button class="edit-btn"><img src="images/icon-edit.svg" alt="">Edit</button>
                 </div>
               </div>
@@ -45,7 +57,7 @@ sendBtn.forEach(btn => {
                     <p class="creation-time">1 month ago</p>
                   </div>
                   <div class="buttons">
-                    <button class="del-btn"><img src="images/icon-delete.svg" alt="">Delete</button>
+                    <button class="del-btn" onclick="delModal()"><img src="images/icon-delete.svg" alt="">Delete</button>
                     <button class="edit-btn"><img src="images/icon-edit.svg" alt="">Edit</button>
                   </div>
                 </div>
@@ -53,7 +65,10 @@ sendBtn.forEach(btn => {
                 </div>
               </div>
             </div>`
-        )
-        userComment.value = ""
-    })
+    )
+    userComment.value = ""
+  })
 })
+
+//delete comment
+// delComment.addEventListener('click', )
